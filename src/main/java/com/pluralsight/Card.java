27ed858fee37f3a6b10ed6698f20265933128b63
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-06 15:25:14 -0400

    Classes added.

--- src/main/java/com/pluralsight/Card.java
+++ src/main/java/com/pluralsight/Card.java
@@ -1,4 +1,62 @@
 package com.pluralsight;
 
 public class Card {
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = false;
+    }
+
+    public String getSuit(){
+        // only return the suit if the card is face up
+        if(isFaceUp){
+            return suit;
+        } else {
+            return "#";
+        }
+    }
+    public String getValue(){
+        // only return the value if the card is face up
+        if(isFaceUp){
+            // this is the string value of the card
+            // i.e. A, K, Q, J, 10, 9 ...
+            return value;
+        } else {
+            return "#";
+        }
+    }
+    public int getPointValue(){
+        // only return the value if the card is face up
+        if(isFaceUp){
+            try {
+                int value = Integer.parseInt(this.value);
+                return value;
+            } catch (NumberFormatException e) {
+
+            }
+            switch (this.value){
+                case "J","Q","K"-> {
+                    return 10;
+                }
+                case  "A" ->
+                {
+                    return 11;
+                }
+                default ->
+                    throw new RuntimeException("Invalid card");
+            }
+        } else {
+            return 0;
+        }
+
+    }
+    public boolean isFaceUp(){
+        return isFaceUp;
+    }
+    public void flip(){
+        isFaceUp = !isFaceUp;
+    }
 }
--- src/main/java/com/pluralsight/Table.java
+++ src/main/java/com/pluralsight/Table.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
 public class Table {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        Hand p1 = new Hand();
+        Hand p2 = new Hand();
+        Hand p3 = new Hand();
+
+        p1.deal(deck.deal());
+        p2.deal(deck.deal());
+        p3.deal(deck.deal());
+
+        p1.deal(deck.deal());
+        p2.deal(deck.deal());
+        p3.deal(deck.deal());
+
+        System.out.println("P1 hand: " +p1.getValue());
+        System.out.println("P1 hand: " +p2.getValue());
+        System.out.println("P1 hand: " +p3.getValue());
+    }
 }
